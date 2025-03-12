4fc3e80ce98e5b252f52b266e8ef30102c27aaa8
ykh <ysw03031@naver.com>
2025-03-13 00:31:37 +0900

    init

--- src/components/index.ts
+++
@@ -1,5 +0,0 @@
-export * as Form from "./Form";
-export * as Button from "./Button";
-export * as Container from "./Container";
-export * as select from "./select";
-export * as InputLabel from "./InputLabel";
